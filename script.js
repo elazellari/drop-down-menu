974f7781858ae98120afa652da67e85318527835
Erjon Lazellari <192823428+elazellari@users.noreply.github.com>
2025-06-24 00:43:21 -0400

    added base code for image carousel

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//hamburger drop down menu functions
 const navbar = document.querySelector(".navbar");
 const toggle = document.querySelector(".drop-down-button");
 let display = false;
@@ -5,4 +6,14 @@ let display = false;
 toggle.addEventListener("click", () => {
   display = !display;
   navbar.style.display = display ? "block" : "none";
+});
+
+//image carousel functions
+
+let img = document.querySelectorAll(".img");
+
+img.forEach((element) => {
+  element.addEventListener("click", () => {
+    element.style.backgroundColor = "blue";
+  });
 });
