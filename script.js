fe94ae60c8107fa03a68329659d9e51b87260036
Erjon Lazellari <192823428+elazellari@users.noreply.github.com>
2025-06-26 20:45:26 -0400

    added image carousel functions

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const navbar = document.querySelector(".navbar");
 const toggle = document.querySelector(".drop-down-button");
 let display = false;
-
+let previousImage = "";
+let currentimage = "";
 toggle.addEventListener("click", () => {
   display = !display;
   navbar.style.display = display ? "block" : "none";
@@ -11,9 +12,23 @@ toggle.addEventListener("click", () => {
 //image carousel functions
 
 let img = document.querySelectorAll(".img");
+let mainImg = document.querySelector(".img.main-img");
+let previous = mainImg;
 
 img.forEach((element) => {
   element.addEventListener("click", () => {
-    element.style.backgroundColor = "blue";
+    previous.classList = element.classList;
+    element.classList = "img main-img";
+    previous = element;
   });
+});
+
+let left = document.querySelector(".left");
+left.addEventListener("click", () => {
+  console.log("left button clicked");
+});
+
+let right = document.querySelector(".right");
+right.addEventListener("click", () => {
+  console.log("right side clicked");
 });
